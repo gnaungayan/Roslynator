d391f43b3d74e4d2e3f4922503c861e468f302ed
Josef Pihrt <josef.pihrt@outlook.com>
2021-02-19 01:49:19 +0100

    x

---
+++ src/Tests/Testing.Common/Testing/TestCode.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.Text;
+using Roslynator.Testing.Text;
+
+namespace Roslynator.Testing
+{
+    public readonly struct TestCode
+    {
+        internal TestCode(string value, ImmutableArray<TextSpan> spans)
+            : this(value, null, spans)
+        {
+        }
+
+        internal TestCode(string value, string expected, ImmutableArray<TextSpan> spans)
+        {
+            Value = value;
+            ExpectedValue = expected;
+            Spans = spans;
+        }
+
+        public string Value { get; }
+
+        public string ExpectedValue { get; }
+
+        public ImmutableArray<TextSpan> Spans { get; }
+
+        //TODO: IComparer<TextSpan>
+        public static TestCode Parse(string value)
+        {
+            return TextProcessor.FindSpansAndRemove(value);
+        }
+
+        public static TestCode Parse(
+            string value,
+            string replacement1,
+            string replacement2 = null)
+        {
+            return TextProcessor.FindSpansAndReplace(value, replacement1, replacement2);
+        }
+    }
+}
